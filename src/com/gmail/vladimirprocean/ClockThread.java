a85ef5870935c8d5ca31c9191fd58a1cdb0b5607
Vladimir Proshin <VlaShin1986@gmail.com>
2023-01-08 19:00:23 +0200

    Initial commit

---
+++ src/com/gmail/vladimirprocean/ClockThread.java
@@ -0,0 +1,31 @@
+package com.gmail.vladimirprocean;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ClockThread implements Runnable {
+    private SimpleDateFormat sdf;
+
+    public ClockThread() {
+        super();
+        this.sdf = new SimpleDateFormat("HH:mm:ss");
+    }
+
+    public ClockThread(String format) {
+        this.sdf = new SimpleDateFormat(format);
+    }
+
+    @Override
+    public void run() {
+        Thread th = Thread.currentThread();
+        while (th.isInterrupted()) {
+            System.out.println(sdf.format(new Date()));
+            try {
+                Thread.sleep(250);
+            } catch (InterruptedException e) {
+                break;
+            }
+        }
+        System.out.println("Clock thread has stopped");
+    }
+}
---
+++ src/com/gmail/vladimirprocean/Main.java
@@ -0,0 +1,7 @@
+package com.gmail.vladimirprocean;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
